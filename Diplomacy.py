95947622ce8e89bc0476e4ad7507f973cfa38b34
Nathan Hardham <nhardham@gmail.com>
2021-07-15 15:03:01 -0500

    debug diplomacy_eval()

--- Diplomacy.py
+++ Diplomacy.py
@@ -1,7 +1,6 @@
-# diplomacy
+# Diplomacy
 
 import sys
-
 
 def diplomacy_read():
 	
@@ -16,6 +15,7 @@ def diplomacy_read():
 		a = line.split()
 		
 		if len(a) < 3:	
+			
 			break
 		
 		else:
@@ -29,6 +29,7 @@ def diplomacy_read():
 			input_list.append(a)
 
 	test = diplomacy_eval(input_list)
+
 	print(test)
 
 
@@ -37,29 +38,80 @@ def diplomacy_eval(a):
 	cities_dict = {}
 
 	for entry in a:
-		
+
 		cities_dict[entry[1]] = {}
 
-		if len(entry) == 3:
+	for entry in a:
+		
+		action = entry[2]
+
+		if action == "Hold":
 
 			cities_dict[entry[1]][entry[0]] = [0]
+		
+		if action == "Support":
 
-	
+			cities_dict[entry[1]][entry[0]] = [0]
+		
+		if action == "Move":
+
+			cities_dict[entry[3]][entry[0]] = [0]
+
 	for entry in a:
 
 		action = entry[2]
 
 		if action == "Support":
 
-			cities_dict[entry[1]][entry[0]] = [0]
+			army = entry[3]
 
+			for i in cities_dict:
+
+				for j in cities_dict[i]:
+
+					if j == army:
+
+						cities_dict[i][army][0] += 1
+
+	war_list = []
+
+	for i in cities_dict:
+		
+		battle_dict = {}
+
+		for j in cities_dict[i]:
+
+			battle_dict[j] = cities_dict[i][j][0]
+
+		battle_dict_copy = battle_dict.copy()
+
+		if len(battle_dict) > 1:
+		
+			max_key_1 = max(battle_dict_copy, key=battle_dict_copy.get)
 			
-	
+			battle_dict_copy.pop(max_key_1)
 
+			max_key_2 = max(battle_dict_copy, key=battle_dict_copy.get)
 
+		battle_dict_copy = battle_dict.copy()
 
+		if max_key_1 == max_key_2:
 
-	return cities_dict
+			for k in battle_dict_copy:
+
+				battle_dict.pop(k)
+
+		else:
+
+			for k in battle_dict_copy:
+				
+				if k != max_key_1:
+
+					battle_dict.pop(k)
+		
+		war_list.append(battle_dict)
+
+	return war_list
 
 if __name__ == '__main__':
 	diplomacy_read()
